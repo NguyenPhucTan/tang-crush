e58db174059339818db18cef35574693203092e0
Nguyen Phuc Tan <51026812+NguyenPhucTan@users.noreply.github.com>
2021-08-30 09:54:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy😍 ',
     btnYes: 'Bé iu Ah nhiều á <33',
-    btnNo: 'Mơ đi nè :3',
+    btnNo: 'Mơ đi nè hehe :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Bởi vì Bé iu anh nhiều lắm á 💘💘💘',
     mess: 'Anh biết mà 🥰. Yêu Bé nhiều nhiều 😘😘',
-    messDesc: '💗Anh chỉ muốn nói là lu Cô Bé của anh💗 .',
+    messDesc: '💗Anh chỉ muốn nói là iu Cô Bé của anh💗 .',
     btnAccept: 'Oce <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
